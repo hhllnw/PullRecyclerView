cff3ac2b059f0d09eac073dbd43e21fb716e188c
hhllnw <920832559@qq.com>
2017-01-10 18:01:36 +0800

    添加动态图

--- app/src/main/java/com/github/hhllnw/pullrecyclerview/MainActivity.java
+++ app/src/main/java/com/github/hhllnw/pullrecyclerview/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements PullRecycler.OnRe
                     e.printStackTrace();
                 }
 
-                for (int i = 0; i < 25; i++) {
+                for (int i = 0; i < 20; i++) {
                     data.add("" + (data.size() + 1));
                 }
 
